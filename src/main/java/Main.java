de4bddfc9b2bda3f44b45f201984d773475b283b
karbudzik <karolinabudzik92@gmail.com>
2020-04-08 17:29:37 +0200

    Clean m ain before merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,6 @@
-import basic.user.User;
-import basic.user.UserDAO;
-
 public class Main {
     public static void main(String[] args) {
         DatabaseContentLoader dbcl = new DatabaseContentLoader();
         dbcl.fillDatabase();
-
-        User user = new User("test@email.pl", "fgfgfgfg");
-        UserDAO dao = new UserDAO();
-        dao.insertUser(user);
     }
 }
