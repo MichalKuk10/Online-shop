a361350d5f76cbebc6edad6b07399e6d2c6d7822
karbudzik <karolinabudzik92@gmail.com>
2020-04-09 17:24:18 +0200

    Partially fixed testing issue

--- src/test/java/connection/ConnectionPropertiesReaderTest.java
+++ src/test/java/connection/ConnectionPropertiesReaderTest.java
@@ -13,7 +13,7 @@ public class ConnectionPropertiesReaderTest {
     @Test
     public void should_read_properties_file_correctly() {
         // given:
-        String filepath = "connection/test.properties";
+        String filepath = "src/test/java/connection/test.properties";
         ConnectionPropertiesReader cpReader = new ConnectionPropertiesReader();
         String expectedPropertyValue = "test.value";
         // when:
@@ -23,24 +23,3 @@ public class ConnectionPropertiesReaderTest {
         assertEquals(testPropertyValue, expectedPropertyValue);
     }
 }
-
-
-
-
-
-
-
-
-    //url = properties.getProperty("db.url");
-    //    public Properties readProperties(String filepath) {
-//        Properties properties = new Properties();
-//        Path path = Paths.get(filepath);
-//
-//        try {
-//            BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
-//            properties.load(reader);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return properties;
-//    }
