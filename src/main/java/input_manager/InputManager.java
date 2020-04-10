8e93045272a69ace1cd060fb0cf13f667de54151
karbudzik <karolinabudzik92@gmail.com>
2020-04-10 15:59:53 +0200

    Add some content to AbstractView and  Input MAnager

--- src/main/java/input_manager/InputManager.java
+++ src/main/java/input_manager/InputManager.java
@@ -1,4 +1,44 @@
 package input_manager;
 
+import view.AbstractView;
+import view.MainControllerView;
+
+import java.util.Scanner;
+
 public class InputManager {
+    private AbstractView view;
+
+    public InputManager() {
+        view = new MainControllerView();
+    }
+
+    public String getStringInput(String message) {
+        view.print(message);
+        Scanner scannerFromUser = new Scanner(System.in);
+
+        return scannerFromUser.nextLine();
+    }
+
+    public int getIntInput(String message) {
+        view.print(message);
+        Scanner scannerFromUser = new Scanner(System.in);
+
+        while(!scannerFromUser.hasNextInt()){
+            view.print("Wrong input! Please insert the correct number");
+            scannerFromUser.next();
+        }
+        return scannerFromUser.nextInt();
+    }
+
+    public int askForMenuOption(String[] menuOptions, String menuTitle) {
+
+        view.printMenu(menuOptions, menuTitle);
+        int statNumber = getIntInput("What do you choose? Type the number.");
+
+        while (statNumber < 1 || statNumber > menuOptions.length) {
+            statNumber = getIntInput(String.format("Wrong input! type the number between 1 and %d",
+                    menuOptions.length));
+        }
+        return statNumber;
+    }
 }
--- src/main/java/view/AbstractView.java
+++ src/main/java/view/AbstractView.java
@@ -1,7 +1,21 @@
 package view;
 
 public abstract class AbstractView {
-    private void print(String message) {
+    public void print(String message) {
         System.out.println(message);
+    }
+
+    public void printMenu(String[] menuItems, String title) {
+        int index = 1;
+        System.out.println(String.format("\n%s:", title.toUpperCase()));
+        for (String item : menuItems) {
+            System.out.println(String.format("%d. %s", index, item));
+            index ++;
+        }
+    }
+
+    public void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
