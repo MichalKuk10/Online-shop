6d8fc9fe2b330b020b40ae80e3a2c2b04012ef71
Miłosz <bryla.milosz@gmail.com>
2020-04-11 22:22:21 +0200

    add Basket, BasketController and BasketControllerView

--- src/main/java/basic/basket/Basket.java
+++ src/main/java/basic/basket/Basket.java
@@ -1,5 +1,61 @@
 package basic.basket;
 
+import basic.product.Product;
+
+import java.util.*;
+
 public class Basket {
+
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
+    private float value;
+
+    public Basket(){
+
+    }
+
+    public void addProduct(Product product, Integer quantity){
+        products.put(product,quantity);
+    }
+
+    public void deleteProduct(String productName){
+        Product productToDelete = null;
+
+        for (Product product_ : products.keySet()){
+            if (product_.getProductName() == productName){
+                productToDelete = product_;
+            }
+        }
+        products.remove(productToDelete);
+    }
+
+    public void changeProductQuantity(String productName, Integer quantity) {
+        Product productToBeChanged = null;
+
+        for (Product product : products.keySet()) {
+            if (product.getProductName() == productName) {
+                productToBeChanged = product;
+            }
+        }
+        products.replace(productToBeChanged, quantity);
+    }
+
+    public void clear(){
+        products.clear();
+    }
+
+    public void calculateBasket() {
+        Iterator<Map.Entry<Product, Integer>> iterator = products.entrySet().iterator();
+
+        while(iterator.hasNext())
+        {
+            Map.Entry<Product, Integer> entry = iterator.next();
+            value += entry.getKey().getprice() * entry.getValue();
+        }
+    }
+
+    public Map<Product, Integer> getproducts(){
+        return products;
+    }
+
 
 }
--- src/main/java/view/BasketControllerView.java
+++ src/main/java/view/BasketControllerView.java
@@ -1,4 +1,39 @@
 package view;
 
+import basic.basket.Basket;
+import basic.product.Product;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class BasketControllerView extends AbstractView {
+
+    BasketControllerView(){
+
+    }
+
+    public void printMenu(String[] menuItems, String title) {
+        int index = 1;
+        System.out.println(String.format("\n%s:", title.toUpperCase()));
+        for (String item : menuItems) {
+            System.out.println(String.format("%d. %s", index, item));
+            index++;
+        }
+    }
+
+    public void showBasket(Basket basket){
+        System.out.println("--Basket--\n");
+        Map<Product, Integer> products = basket.getproducts();
+        Iterator<Map.Entry<Product, Integer>> iterator = products.entrySet().iterator();
+        int i = 1;
+        while(iterator.hasNext())
+        {
+            Map.Entry<Product, Integer> entry = iterator.next();
+            System.out.println(i + ". " + entry.getKey().getProductName() + " | quantity: " + entry.getValue() + " amount: " + (entry.getKey().getProductName() * entry.getValue()) + "\n");
+        }
+
+    }
+
+}
 }
